eb6261509949c50bc1f1e0860431aae22125b5bf
Alex <90933044+0x3alex@users.noreply.github.com>
2024-05-12 23:13:44 +0200

    comments

--- gc_test.go
+++ gc_test.go
@@ -6,23 +6,42 @@ import (
 )
 
 func TestCache(t *testing.T) {
-	gc := New(20*time.Second, 100)
+	/*
+		Create a new cache with a cleaing interval of 5 minutes and an buffer
+		of 100 actions
+	*/
+	gc := New(5*time.Minute, 100)
 	gc.Run()
 	if gc == nil {
 		t.Fatal("GC is nil")
 	}
 	t.Log("GC init ok!")
-	gc.Set("hello", "world", 1*time.Minute)
+	/*
+		Store the pair ("hello", "world") with a time to live of the cache cleaning interval
+	*/
+	gc.Set("hello", "world", gc.DefaultTTL)
+	/*
+		Get a value.
+		@param ok - bool, if the value was found
+		@param value - the value
+	*/
 	if ok, value := gc.Get("hello"); !ok || value.(string) != "world" {
 		t.Fatal("Could not retrieve value or value was not correct")
 	}
 	t.Log("GC set ok!")
-	gc.Set("hello", "mars", 10*time.Second)
+	/*
+		Overwrite the pair ("hello", "world") with the pair ("hello", "mars"), which
+		never expires and won't be cleaned
+	*/
+	gc.SetNoTTL("hello", "mars")
 	if ok, value := gc.Get("hello"); !ok || value.(string) != "mars" {
 		t.Fatal("Could not retrieve value or value was not correct")
 	}
 	t.Log("GC set ok!")
 	gc.Set("deleteMe", "lol", 2*time.Minute)
+	/*
+		Delete an key-value-pair manually
+	*/
 	gc.Delete("deleteMe")
 	if ok, _ := gc.Get("deleteMe"); ok {
 		t.Fatal("Deleted value is still there")
